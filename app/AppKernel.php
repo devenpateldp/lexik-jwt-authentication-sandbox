9312bc1563df18199f92d0c121f5d29352cd2037
Prakash Admane <prakash.admane@theuniprogroup.com>
2018-08-02 15:35:31 +0000

    symfony-3.4: Lexik bundle chanes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+
             new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
         ];
 
